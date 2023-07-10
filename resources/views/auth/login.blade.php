352ce8a3f07363fcdd14e2a3b30a37d357ea9a35
mil-pjlp2021 <emrilizhaq@gmail.com>
2023-07-10 10:55:54 +0700

    tipe A

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,8 +44,12 @@
                 </x-button>
             </div>
 
+            <div class="flex items-center justify-end mt-4">
+                <a class="px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-500" href="{{ route('register') }}">
+                    {{ __('Register') }}
+                </a>
+            </div>
             <div class="flex items-center justify-center mt-4">
-
                 <a href="{{ url(''); }}/auth/google" class="mr-2 inline-block items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-400 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
                     Login  Google
                 </a>
